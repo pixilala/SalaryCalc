3996057215ff13a2c0f5c93c19417230af1f7da9
John Williams <ponix4k@gmail.com>
2019-09-30 11:03:35 +0100

    update Main.py
    Added PAYE Functionality to the calc

--- Main.py
+++ Main.py
@@ -1,19 +1,59 @@
+
 #!/usr/bin/python3
 
 salaried = input('Are you on a yearly salary?: ')
+taxCode = input('What is your current Tax Code: ')
 
-if salaried == True:
-    overHourlyRate = input('Enter hourly rate: ')
-    salary = input('Enter Salary: ')
-    hoursPerWeek = input('Enter Weekly Hours :')
+
+if salaried == 'yes':
+   # overHourlyRate = input('Enter hourly rate: ')
+    grossSalary = input('Enter Salary: ')
+    hoursPerWeek = input('Enter Weekly Hours: ')
 else:
     hoursPerWeek = input('Enter Weekly Hours: ')
     hourlyRate = input('Enter Hourly Rate: ')
-    payPeriodsPerYear = input('How Many Pay Periods Per Year :')
-    print(hoursPerWeek)
-    weeklyPay = 4.0 * float(hoursPerWeek)  
-    print('weekly salary pre tax: '+ str(weeklyPay))
-    Salary = float(weeklyPay) * float(payPeriodsPerYear)
-    print('Yearly salary pre tax: '+ str(Salary))
-#hourlyRate = float(salary) / 52 / float(hoursPerWeek)
-#print(hourlyRate)
+    weeklyPay = float(hoursPerWeek) * 4
+    print('weekly gross pre tax: '+ str(weeklyPay))
+    Salary = float(weeklyPay) * 13
+    print('Yearly gross pre tax: '+ str(grossSalary))
+
+if taxCode == 'BR':
+                 print('You are using an emergency tax code')
+                 taxCodeStr = 'BR'
+else:
+                 print('You Are using a personal allowance code')
+                 taxCodeStr = taxCode[:-1] # the [:-1] removes the last character from the string
+                 print(taxCodeStr)
+                 taxCodeAllowance = int(taxCodeStr)*10
+                 print('Your Tax Free Allowance is: ' + str(taxCodeAllowance))
+
+if taxCodeStr == 'BR':
+                taxFreeAllowance = 0
+else:
+                taxFreeAllowance = taxCodeAllowance
+#
+grossSalaryInt = int(grossSalary)
+#taxcalculation
+if grossSalaryInt <= 12500 and grossSalaryInt < 125001:
+                 taxRate = 0.00
+                 print('You have not reached your monthly allowance')
+elif grossSalaryInt <= 12501 and grossSalaryInt < 50000:
+                 taxRate = 0.20
+                 print('You are on Basic Rate (20%)')
+elif grossSalaryInt <= 50001 and grossSalaryInt < 150000:
+                 taxRate = 0.40
+                 print('You are on Higher Rate Tax (40%)')
+elif grossSalaryInt <= 150000:
+                 taxRate = 0.45
+                 print('You are on Additonal Rate Tax (45%)')
+
+if taxCodeStr == 'BR':
+    deductableAmount = grossSalaryInt
+    print(deductableAmount)
+else:
+    deductableAmount = grossSalaryInt * taxRate
+    print(deductableAmount)
+
+totalNetSalary = grossSalaryInt - deductableAmountprint
+('Your Total Net Salary: '+str(totalNetSalary))
+
