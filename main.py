0dfa6be6b59f69fdbc781169850a1dac95f479fe
pixilala <charlotte.carter86@gmail.com>
2019-10-28 13:25:55 +0000

    started on deductions too, hourly rate doesn't really change as the important part of deductions is

--- main.py
+++ main.py
@@ -12,13 +12,35 @@ def calculate_hourly_rate(salary, hours_in_working_week):
 	hours_worked = hours_in_working_week*48
 	return salary/hours_worked
 
+def calculate_pension_contributions(salary, contribution_percent):
+	return salary * (contribution_percent/100)
+
+def calculate_weekly_pension_contributions(salary, contribution_percent):
+	return calculate_pension_contributions(salary, contribution_percent)/48
+
+def print_info(wage_params, deductions):
+	weekly = calculate_weekly_from_salary(wage_params[0], wage_params[2])
+	hourly = calculate_hourly_rate(wage_params[0], wage_params[1])
+	if (deductions):
+		print("with deductions:\n")
+		weekly = weekly - calculate_weekly_pension_contributions(wage_params[0], 3)
+	else:
+		print("without deductions:")
+
+	print("you earn {0} a week\n and {1} an hour ".format(weekly, hourly))
+
 def main():
+	wage_params = []
 	salary = float(input("\nenter salary: "))
 	weekly_hours = float(input("enter weekly hours: "))
 	monthly = bool(input("are you paid monthly as opposed to every 4 weeks: "))
+	wage_params.append(salary)
+	wage_params.append(weekly_hours)
+	wage_params.append(monthly)
 
-	print("hourly rate is:{0}".format(calculate_hourly_rate(salary, weekly_hours)))
-	print("you get {0} a week".format(calculate_weekly_from_salary(salary, monthly)))
+	print(wage_params)
+
+	print_info(wage_params, bool(input("show deductions: ")))
 
 if __name__ == "__main__":
 	main()
